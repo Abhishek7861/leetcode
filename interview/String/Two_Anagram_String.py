ade5f60e03f3375f7eed55bb14ac25f663863ae5
ABHISHEK SINGH <38339710+Abhishek7861@users.noreply.github.com>
2022-04-15 21:54:40 +0530

    Update Two_Anagram_String.py

--- interview/String/Two_Anagram_String.py
+++ interview/String/Two_Anagram_String.py
@@ -1,1 +1,13 @@
 # How do you check if two strings are anagrams of each other?
+
+# Approach 1:
+# sort both the strings and compare them. Time Complexity: O(nlogn)
+
+# Approach 2:
+# use two hashMap, traverse both the string and maintain character count. Compare both the hashMap
+
+# Approach 3:
+# use single hashMap, traverse one string and store character count in hashMap. Traverse second string and subtract count for each character.
+# if all the characters have count = 0 then strings are anagram else not.
+
+
